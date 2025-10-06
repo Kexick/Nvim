d11b047fc8de30bbb2bc34d21017d3e8d8430c04
Kexick <trogister.to@gmail.com>
2025-10-07 04:58:20 +0600

    add terminal options

--- lua/utils/terminal.lua
+++ lua/utils/terminal.lua
@@ -1,15 +1,32 @@
--- Fixed-size bottom terminal and 'term here' command
-vim.api.nvim_create_user_command("FixedTerminal", function(opts)
-  local height = tonumber(opts.args) or 12
-  vim.cmd(string.format("%dsplit | terminal", height))
-  vim.cmd("startinsert")
-end, { nargs = "?" })
 
-vim.api.nvim_create_user_command("TermHere", function(opts)
-  local h = tonumber(opts.args) or 10
+local term = { win = nil, buf = nil }
+
+function _G.TermToggle(height)
+  height = tonumber(height) or 12
+
+  if term.win and vim.api.nvim_win_is_valid(term.win) then
+    vim.api.nvim_win_close(term.win, true)
+    term.win = nil
+    return
+  end
+
   local dir = vim.fn.expand("%:p:h")
   if dir == "" then dir = vim.fn.getcwd() end
-  vim.cmd(("lcd %s | botright %dsplit | terminal"):format(vim.fn.fnameescape(dir), h))
+  vim.cmd(("lcd %s"):format(vim.fn.fnameescape(dir)))
+
+  if term.buf and vim.api.nvim_buf_is_valid(term.buf) then
+    vim.cmd(("botright %dsplit"):format(height))
+    vim.api.nvim_win_set_buf(0, term.buf)
+  else
+    vim.cmd(("botright %dsplit | terminal"):format(height))
+    term.buf = vim.api.nvim_get_current_buf()
+  end
+
   vim.cmd("setlocal winfixheight")
   vim.cmd("startinsert")
-end, { nargs = "?" })
+  term.win = vim.api.nvim_get_current_win()
+end
+
+vim.keymap.set("n", "<leader>t", function() TermToggle(12) end,
+  { desc = "Toggle terminal (in file dir, fixed height)", silent = true })
+
